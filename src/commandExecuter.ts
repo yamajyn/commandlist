0d8c8d0d5d21d686955c4b342c6043e08468a40a
yamajyn <j.yama.japan@gmail.com>
2022-08-13 22:51:27 +0900

    feat: 時間を計測する

--- src/commandExecuter.ts
+++ src/commandExecuter.ts
@@ -7,22 +7,25 @@ export class CommandExecuter {
 
   constructor(viewId: string, context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand(`${viewId}.execute`, (element: Entry) => this.executeCommand(element)));
+    context.subscriptions.push(vscode.commands.registerCommand(`${viewId}.watch`, (element: Entry) => this.watchCommandTime(element)));
   }
 
-  private executeCommand(element: Entry){
+  private executeCommand(element: Entry, needsWatchTime: boolean = false) {
     const command: Command = JSON.parse(fs.readFileSync(element.uri.fsPath, 'utf8'));
     this.ensureTerminalExists();
-    if(vscode.window.terminals.length === 1){
+    if (vscode.window.terminals.length === 1) {
       const terminal = vscode.window.terminals[0];
       terminal.show();
       console.log(`execute $ ${command.script}`);
       terminal.sendText(command.script ? command.script : '');
+      if (needsWatchTime) { this.startWatch(element, command) }
     } else {
       this.selectTerminal().then(terminal => {
         if (terminal) {
           terminal.show();
           console.log(`execute $ ${command.script}`);
           terminal.sendText(command.script ? command.script : '');
+          if (needsWatchTime) { this.startWatch(element, command) }
         } else {
           console.error('Selected Terminal is not exist');
           vscode.window.showErrorMessage('Sorry, Unexpected error has occurred.');
@@ -46,11 +49,45 @@ export class CommandExecuter {
       return item ? item.terminal : undefined;
     });
   }
-  
+
   private ensureTerminalExists(): void {
     if (vscode.window.terminals.length === 0) {
       vscode.window.createTerminal('Command List');
     }
   }
-  
+
+  private async watchCommandTime(element: Entry) {
+    const value = await vscode.window.showInformationMessage(
+      "[BETA] Stopwatch. Do you want to measure time? If recorded, a progress bar will appear next time.",
+      "Start",
+      "Cancel"
+    );
+    if (value === 'Start') {
+      this.executeCommand(element, true);
+    }
+  }
+
+  private async startWatch(element: Entry, command: Command) {
+    let start = new Date();
+    const value = await vscode.window.showInformationMessage(
+      "Record the execution time",
+      "Completed. Record time",
+      "Cancel"
+    );
+    if (value === 'Completed. Record time') {
+      let end = new Date();
+      let time = (end.getTime() - start.getTime()) / 1000
+      console.log(time);
+      command.time = time;
+      await fs.writeFileSync(element.uri.fsPath, this.stringToUnit8Array(JSON.stringify(command)))
+      await vscode.window.showInformationMessage(
+        "Congratulations👏 The progress bar will appear next time🥳",
+      )
+    }
+  }
+
+  private stringToUnit8Array(s: string): Uint8Array {
+    return Uint8Array.from(Buffer.from(s));
+  }
+
 }
